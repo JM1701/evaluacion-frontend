4f8666aa8ebf95d0558a02b4dc552e862d08c957
José Mirabal <josemirabal1701@gmail.com>
2021-05-21 12:41:30 -0430

    Comentarios en el  JS Solicitud R4

--- code/R4/js/scripts.js
+++ code/R4/js/scripts.js
@@ -1,11 +1,17 @@
+/* Variables */
 const dataTable = document.querySelector("#table-data");
 
+/* Petición a la API de MediaStack */
 fetch("http://api.mediastack.com/v1/news?access_key=d2de2a96cf411f9aabba1773035ee4d2&keywords=tennis&countries=us")
   .then(res => res.json())
   .then(info => {
+    // Recorre el array de objetos
     info.data.forEach(dataNew => {
+      // Crea un tr en el HTML
       let tr = document.createElement("tr");
-      tr.classList.add("table-row")
+      // Asigna la clase table-row al tr
+      tr.classList.add("table-row");
+      // Inserta los td con los datos obtenidos de la API en el HTML
       tr.innerHTML = `
         <td>${dataNew.author}</td>
         <td>${dataNew.title}</td>
@@ -14,6 +20,7 @@ fetch("http://api.mediastack.com/v1/news?access_key=d2de2a96cf411f9aabba1773035e
         <td>${dataNew.language}</td>
         <td>${dataNew.country}</td>
       `;
+      // Inserta el tr en el tbody del HTML
       dataTable.appendChild(tr);
     });
   });
